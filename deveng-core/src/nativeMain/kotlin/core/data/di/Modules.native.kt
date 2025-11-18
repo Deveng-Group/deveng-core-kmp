672140bf9cd7b3ee4cc243c4a7ad030033569ad0
furkanturkn <furkan.trkn1@gmail.com>
2025-11-18 23:15:04 +0300

    Remove p

--- deveng-core/src/nativeMain/kotlin/core/data/di/Modules.native.kt
+++
@@ -1,9 +0,0 @@
-package core.data.di
-
-import core.util.multiplatform.MultiPlatformUtils
-import org.koin.core.module.dsl.singleOf
-import org.koin.dsl.module
-
-actual val platformModule = module {
-    singleOf(::MultiPlatformUtils)
-}
