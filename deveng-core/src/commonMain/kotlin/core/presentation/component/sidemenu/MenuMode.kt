c06156cae2dd0045b533456d1585430dfb0ec976
berkcelik99 <berk.celik@sekompos.com>
2025-12-18 00:21:11 +0300

    Add horizontal mode and alignment options to CustomSideMenu

---
+++ deveng-core/src/commonMain/kotlin/core/presentation/component/sidemenu/MenuMode.kt
@@ -0,0 +1,17 @@
+package core.presentation.component.sidemenu
+
+/**
+ * Display mode options for the menu component.
+ */
+enum class MenuMode {
+    /**
+     * Vertical side menu mode with expand/collapse functionality.
+     */
+    Vertical,
+    
+    /**
+     * Horizontal header mode, always expanded and fixed.
+     */
+    Horizontal
+}
+
